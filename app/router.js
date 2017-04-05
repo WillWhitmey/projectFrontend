0ba12ad932b837928f95ea9a9dc7a28294873d14
WillWhitmey <will.whitmey@gojip2p.com>
2017-04-05 15:12:29 +0100

    Working: change between different applications

--- app/router.js
+++ app/router.js
@@ -10,6 +10,20 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('rentals');
+  this.route('api-application');
+  this.route('admin-api-application');
+  this.route('customer-service-application');
+  this.route('execution-settlement-application');
+  this.route('investor-application');
+  this.route('isa-application');
+  this.route('payments-application');
+  this.route('security-application');
+  this.route('workflow-manager-application');
+  this.route('reporting-billing-application');
+  this.route('admin-ui');
+  this.route('insight-ui');
+  this.route('investments-ui');
+  this.route('website-ui');
 });
 
 export default Router;
--- app/routes/api-application.js
+++ app/routes/api-application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return $.ajax({
-            url: "https://lycium.herokuapp.com/changes/application/" + "ADMIN_UI" + "?in=TEST&notIn=PRODUCTION",
+            url: "https://lycium.herokuapp.com/changes/application/" + "API_APPLICATION" + "?in=TEST&notIn=PRODUCTION",
             type: 'GET',
             success: function(res) {
               let data = res;
