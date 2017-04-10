8f09664f8977867af5a4b450a361c15a51218786
WillWhitmey <will.whitmey@gojip2p.com>
2017-04-10 18:06:53 +0100

    Update changes.js

--- app/adapters/changes.js
+++ app/adapters/changes.js
@@ -2,7 +2,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   query(store, type, query) {
-    let url = "https://lycium.herokuapp.com/changes/application/" + query.applicationName + "?in=TEST&notIn=PRODUCTION"
+    let url = "https://lycium-service.herokuapp.com/changes/application/" + query.applicationName + "?in=TEST&notIn=PRODUCTION"
 
     return this.ajax(url, 'GET' );
   },
