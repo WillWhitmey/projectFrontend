f0ff9f1318dca4dc860dc6b336e99bcd2c47404f
WillWhitmey <will.whitmey@gojip2p.com>
2017-08-18 16:39:44 +0100

    removed a few files and am listing stocks and making post

--- app/components/stocks-list.js
+++ app/components/stocks-list.js
@@ -11,9 +11,45 @@ export default Ember.Component.extend({
   init() {
     this._super(...arguments);
     $.getJSON('http://localhost:4567/stocks/').then(data => {
+      console.log(data);
       this.set('stocks', data);
-      console.log(this.get('stocks'));
     });
+  },
+
+  actions: {
+    buyStock() {
+      $.ajax({
+        type: "PUT",
+        url: "http://localhost:4567/stocks/update/",
+        ContentType: "application/json; charset=utf-8",
+        dataType: "json",
+        crossOrigin: true,
+        cache: false,
+        // data: JSON.stringify({ "name": "Apple", "price": "22" })
+        body: {
+          "id": "1",
+          "price": "7500"
+        }
+      })
+    }
   }
 
 });
+
+
+// $.ajax({
+//
+//     url: apiurl,
+//     type: "POST",
+//     ContentType: "application/json; charset=utf-8",
+//     data: Lead,
+//     crossOrigin: true,
+//     dataType: "json",
+//     cache: false,
+//     complete: function (res) {
+//         alert("Data Added Successfully");
+//     },
+//     error: function (xhr) {
+//         alert("Error");
+//     }
+// })
